781931b13ca349446dc5b0f0687b05feda1330ef
WenXuanLee <speechless0922@gmail.com>
2019-05-15 14:15:54 +1200

    import Vue.js via CDN, style intro part and select button, implement button click event

--- js/index.js
+++ js/index.js
@@ -5,3 +5,42 @@
  * add to minicart and update the num in cart
 */
 
+new Vue ({
+    el: '#app',
+    data: {
+      cartTotalCount: 4,
+      selectedSize: '',
+      isSelected: false,
+      isShowMiniCart: false,
+      isError: false,
+      sizeOptions: {
+        S: false,
+        M: false,
+        L: false
+      }
+    },
+    computed: {
+    },
+    methods: {
+      selectSize(input) {
+        /* in case v-for render empty button */
+        if(this.selectedSize) this.sizeOptions[this.selectedSize] = false
+        if(input !== this.selectedSize ) {
+          this.isSelected = true
+          this.sizeOptions[input] = true   
+          this.selectedSize = input  
+        } else {
+          this.isSelected = false
+          this.sizeOptions[input] = false 
+          this.selectedSize = ''
+        }
+      },
+      checkAddToCart() {
+        if(this.isSelected && this.selectedSize) {
+          this.isError = false
+        } else {
+          this.isError = true
+        }
+      }
+    },
+  })
